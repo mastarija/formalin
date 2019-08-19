67aacd7a54b9a848dc84db1cdcf2f5b54052177b
mastarija <master@mastarija.com>
2019-08-19 16:50:18 +0200

    finished the label / attribute parser

---
+++ lib/Formalin/LabelParser.hs
@@ -0,0 +1,33 @@
+module Formalin.LabelParser where
+--
+import Prelude hiding ( takeWhile )
+--
+import Data.Text ( Text )
+import Data.List ( notElem )
+import Data.String ( fromString )
+import Control.Applicative ( many, optional )
+import Data.Attoparsec.Text ( Parser, char, string, choice, takeWhile, takeWhile1, endOfInput )
+--
+
+types :: [ Text ]
+types = fmap fromString [ "Null", "Bool", "Binary", "Number", "String", "Vector" ]
+
+valueType :: Parser Text
+valueType = choice $ fmap string types
+
+simple :: Char -> Bool
+simple = flip notElem "[:]"
+
+simpleLabel :: Parser Text
+simpleLabel = takeWhile1 simple
+
+nestedLabel :: Parser Text
+nestedLabel = char '[' *> takeWhile simple <* char ']'
+
+labelParser :: Parser ( [ Text ], Maybe Text )
+labelParser = do
+  simple <- simpleLabel
+  nested <- many nestedLabel
+  mcolon <- optional $ char ':'
+  mvtype <- maybe ( pure Nothing ) ( const $ Just <$> valueType ) mcolon
+  endOfInput >> pure ( simple : nested, mvtype )
