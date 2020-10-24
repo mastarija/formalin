980629ad43db5adc7bd25dec718bff7db8f5e7c0
Maštarija <51950685+mastarija@users.noreply.github.com>
2020-10-24 22:41:50 +0200

    cleanup

--- lib/Formalin.hs
+++ lib/Formalin.hs
@@ -1,28 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
---
 module Formalin where
 --
-import Data.Foldable       ( foldl' )
-import Opaque              ( Opaque (..), OVector, ORecord, OLabel )
-import Data.Maybe          ( isJust, fromMaybe )
-import Data.Text           ( Text )
-import Control.Applicative ( (<|>) )
-import Formalin.Parser     ( labelParser )
-import Data.HashMap.Strict ( alter )
-import Text.Read           ( readMaybe )
-import Data.Scientific     ( Scientific )
+import Data.Opaque ( Opaque (..) )
 --
-
-builder :: Opaque -> [ ( [ OLabel ], Opaque ) ] -> Opaque
-builder = foldl' $ flip $ uncurry embedder
-
-embedder :: [ OLabel ] -> Opaque -> Opaque -> Opaque
-embedder []   val _ = val
-embedder lbls val src = ORecord $ recorder lbls val $ case src of
-  ORecord rec -> rec
-  otherwise   -> mempty
-
-recorder :: [ OLabel ] -> Opaque -> ORecord -> ORecord
-recorder []           _   rec = rec
-recorder ( lbl:lbls ) val rec = alter
-  ( Just . embedder lbls val . fromMaybe ONull ) lbl rec
